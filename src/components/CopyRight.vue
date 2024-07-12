634e9a18b69b20bda30b007072a4c7494fb38191
VanTien2003 <phungtien07062003@gmail.com>
2024-07-13 00:01:24 +0700

    tao mot con mini game pokemon

---
+++ src/components/CopyRight.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="coppyright">
+    <p>
+      This game owned by Van Tien in Vue 3 -
+      <a href="#">view here</a>
+    </p>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
+}
+</style>
